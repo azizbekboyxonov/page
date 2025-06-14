5d1f68ce2b3135e19855e6e50468aff989791903
azizbek <azizbek13092008@gmail.com>
2025-06-14 20:42:20 +0500

    https://github.com/azizbekboyxonov/page2.git

--- home/models.py
+++ home/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Subject(models.Model):
+    name = models.CharField(max_length=100)
+
+    def str(self):
+        return self.name
+
+
+class Student(models.Model):
+    full_name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    grade = models.CharField(max_length=20)
+    subjects = models.ManyToManyField(Subject, related_name='student')
+
+    def str(self):
+        return self.full_name
--- home/views.py
+++ home/views.py
@@ -6,3 +6,9 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def Student(request):
+    return render(request, 'Student.html')
+
+def Subject(request):
+    return render(request, 'subject.html')
